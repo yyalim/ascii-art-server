2ac3afea3a71839f8e2754ff7240ed7b37df3037
Yusuf YALIM <yusufyalim@Yusufs-MacBook-Pro.local>
2024-11-13 17:51:01 +0100

    Add healtcheck endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import fileRoutes from './controllers/fileController';
+import healthCheckRoutes from './controllers/healthCheck';
 import errorHandler from './middleware/errorHandler';
 import notFoundHandler from './middleware/notFoundHandler';
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use('/upload', fileRoutes);
+app.use('/healthCheck', healthCheckRoutes);
 app.use(notFoundHandler);
 app.use(errorHandler);
 
